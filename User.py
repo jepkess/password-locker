b24650d53030c7646b18797cf44c6681fa8a3798
jepkess <biivincent1997@gmail.com>
2021-10-22 21:29:07 +0300

    create an empty user list

--- User.py
+++ User.py
@@ -2,4 +2,15 @@ class User:
     """
     class that generates new instances of a user
     """
-    
+    User_list=[] # Empty user list
+    def __init__(self,username,password):
+        User.username=username
+        User.password=password
+    def save_User(self):
+        """
+        method that saves the user to the list
+        """   
+        User.user_list(self).append(self)
+
+        
+          
