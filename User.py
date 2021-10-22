67b2aeb425a6ea6e969b3440cbcbefd3dff1af1d
jepkess <biivincent1997@gmail.com>
2021-10-22 23:11:02 +0300

    import a random and string in the user.py file

--- User.py
+++ User.py
@@ -1,3 +1,5 @@
+import random
+import string
 class User:
     """
     class that generates new instances of a user
@@ -40,6 +42,47 @@ class Details:
             if(user.username == username and user.password == password):
                 a_user == user.username
                 return a_user
+
+    def save_User_Details(self):
+        """
+        a method that saves the user details to the details list.
+        """
+        Details.details_list.append(self)
+
+    def delete_details(self):
+        """
+        method that details the details in the details list
+        """  
+        Details.details_list.remove(self)
+
+    @classmethod
+    def find_by_number(cls, account):
+        """
+        a method that find account number and return the password that matches with the number
+        """  
+
+        for details in Details.details_listcls.credentials_list:
+            if(details.account_number == account):
+                return details 
+
+    @classmethod
+    def details_exists(cls,account):
+        """
+        this method checks whether the details in the details list exists and return 
+        a boolean.
+        """
+        for detail in cls.details_list:
+            if detail.account == account:
+                return True
+        return False
+    def generates_password(self):
+        """
+        method that generates random password of mix letters and numbers
+        """   
+        password = string.ascii_uppercase + string.ascii_lowercase
+        return ''.join(random.choice(password) for i in range(1,9))
+
+
    
 
             
