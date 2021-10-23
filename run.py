5c2841150a703ce2f47b419d832bfa4291381cd6
jepkess <biivincent1997@gmail.com>
2021-10-23 15:10:44 +0300

    verify the user login details

--- run.py
+++ run.py
@@ -3,19 +3,24 @@ from User import User, Details
 # creating a new user
 def create_user(username, password):
     """"
-    method that creates a new user
+    Function that creates a new user
     """
     new_user=User(username,password)
     return new_user
 
 def save_user(user):
     """
-    method that save the saves the user
+    Function that save the saves the user
     """ 
     user.save_user()
 def display_user(user):
     """
-    a method that displays already save user
+    Function that displays already save user
     """
     return User.display_user(user)
-           
+def login_user(password, username):
+    """
+    a function that if the user has already exists
+    """  
+    checked_user = Details.verify_user(password, username)
+    return checked_user
