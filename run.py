3c0b13e423ddebdfb4eb1dc713df3b14b43f6525
jepkess <biivincent1997@gmail.com>
2021-10-23 15:20:46 +0300

    create a function that save the user details

--- run.py
+++ run.py
@@ -18,9 +18,25 @@ def display_user(user):
     Function that displays already save user
     """
     return User.display_user(user)
+    # verify the user login details
 def login_user(password, username):
     """
     a function that if the user has already exists
     """  
     checked_user = Details.verify_user(password, username)
     return checked_user
+
+    #creating new details
+
+def create_new_detail(account,username,password):
+    """
+    a function that create new details.
+    """ 
+    new_detail= Details(account,username,password)  
+    return new_detail
+
+def save_details(details):
+    """
+    function that saves the created details
+    """
+    details.save_user_details()   
